6b455ab346b626475eafdad1d6202e859e637dd0
Willem van Duijn <willemvanduijn.dev@gmail.com>
2023-02-20 21:50:38 +0100

    fix: fixed build error

--- services/date.ts
+++ services/date.ts
@@ -1,17 +1,18 @@
 // can add weekday: long if needed
-const options = {
-  weekday: 'long',
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-};
-
 export const getTodayDate = () => new Date().toISOString().slice(0, 10);
 export const getReadableDate = () =>
-  new Date().toLocaleDateString('nl-NL', options);
-
+  new Date().toLocaleDateString('nl-NL', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 
 export const transformDate = (date: Date) => {
-  return new Date(date).toLocaleDateString('nl-NL', options);
-}
-
+  return new Date(date).toLocaleDateString('nl-NL', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+};
